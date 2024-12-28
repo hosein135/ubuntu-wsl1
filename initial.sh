58ccd88502e48dcabf20e061f476010300a8434f
Hossein Ansari-Jafari <hoseinansari135@gmail.com>
2024-12-29 00:31:17 +0330

    removed items related to zsh and github cli added

--- initial.sh
+++ initial.sh
@@ -38,6 +38,18 @@ printf "${YELLOW}Installing git${NC}\n";
 sleep $delay_after_message;
 sudo apt install git sed -y
 sleep 2;
+#Install github cli
+printf "${YELLOW}github cli${NC}\n";
+sleep $delay_after_message;
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+        && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+        && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
+sleep 2;
 #change git default branch name
 printf "${YELLOW}change git default branch name to main${NC}\n";
 sleep $delay_after_message;
@@ -47,10 +59,8 @@ sleep 2;
 printf "${YELLOW}Installing asdf${NC}\n";
 sleep $delay_after_message;
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
-echo ". $HOME/.asdf/asdf.sh" >> ~/.zshrc
 echo ". $HOME/.asdf/asdf.sh" >> ~/.bashrc
 source ~/.bashrc
-source ~/.zshrc
 asdf plugin add nodejs
 sed -i 's/for client in aria2c curl wget; do/for client in curl wget; do/' ~/.asdf/plugins/nodejs/.node-build/bin/node-build
 sleep 2;
